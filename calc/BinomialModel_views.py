573de6e61eeca40c9c2f3720382891c52fc45f25
Pavan Adari <94852934+JustADude29@users.noreply.github.com>
2023-03-16 13:54:40 +0530

    balls

--- calc/BinomialModel_views.py
+++ calc/BinomialModel_views.py
@@ -10,20 +10,23 @@ def binomial_tree(S0, K, T, r, sigma, N, call_put):
     dt = T/N
     u = np.exp(sigma * np.sqrt(dt))
     d = np.exp(-sigma * np.sqrt(dt))
+    print(u)
+    print(d)
     p = (np.exp(r*dt) - d)/(u - d)
+    print(p)
     c = 0
     for i in range(N+1):
-        #node_prob = comb(N, i)*(pow(p, i))*(pow((1-p), (N-i)))
-        node_prob = comb(N, i) * (p)**i * (1-p)**(N-i)
-        #ST = S0*(pow(u, i))*(pow(d, N-i))
-        ST = S0 * (u)**i * (d)**(N-i)
+        node_prob = comb(N, i)*(pow(p, i))*(pow((1-p), (N-i)))
+        #node_prob = comb(N, i) * (p)**i * (1-p)**(N-i)
+        ST = S0*(pow(u, i))*(pow(d, N-i))
+        #ST = S0 * (u)**i * (d)**(N-i)
         if call_put == 'C':
             c += max(ST-K, 0) * node_prob
         elif call_put == 'P':
             c += max(K-ST, 0) * node_prob
         else:
             raise ValueError("Must be 'C' or 'P'")
-    return c * np.exp(-r*T)
+    return round(c * np.exp(-r*T), 4)
 
 
 def binomialIndex(request):
